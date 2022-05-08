95da7e3b17002c7582cfecaed51790f496ba6562
Flavio Teixeira <flavio.teixeira@take.net>
2022-05-08 14:50:09 -0300

    Feat: Working with React.Fragment

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import React from "react";
 import "./index.css";
 import First from "./components/basics/First";
 import WithParameter from "./components/basics/WithParameter";
+import Fragment from "./components/basics/Fragment";
 
 const el = document.getElementById("root");
 ReactDOM.render(
   <div>
     <First />
     <WithParameter title="second component" subtitle="Very nice!" />
+    <Fragment />
   </div>,
   el
 );
