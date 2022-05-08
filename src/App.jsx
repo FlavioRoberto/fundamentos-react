e7cda0987a8679ea9c7bf2fddd211af23bd78cb2
Flavio Teixeira <flavio.teixeira@take.net>
2022-05-08 15:06:41 -0300

    Feat: ensure random number component

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import First from "./components/basics/First";
 import WithParameter from "./components/basics/WithParameter";
 import Fragment from "./components/basics/Fragment";
+import Random from "./components/basics/Random";
 
 const App = () => (
   <div id="app">
     <h1>React essentials</h1>
+    <Random min={1} max={5} />
     <First />
     <WithParameter title="second component" subtitle="Very nice!" />
     <Fragment />
---
+++ src/components/basics/Random.jsx
@@ -0,0 +1,13 @@
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const Random = (props) => {
+  let { min, max } = props;
+  const randomNumber = getRandomInt(min, max);
+  return <h2>{randomNumber}</h2>;
+};
+
+export default Random;
