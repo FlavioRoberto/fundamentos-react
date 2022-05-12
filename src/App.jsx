d45a81581d1e54c4fc64b23fda3599d23e146564
Flavio Teixeira <flavio.teixeira@take.net>
2022-05-12 19:41:04 -0300

    Fix: Adjusted styles to align cards

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,29 @@ import First from "./components/basics/First";
 import WithParameter from "./components/basics/WithParameter";
 import Fragment from "./components/basics/Fragment";
 import Random from "./components/basics/Random";
+import Card from "./components/layouts/Card/Card";
+import "./App.css";
 
 const App = () => (
   <div id="app">
     <h1>React essentials</h1>
-    <Random min={1} max={5} />
-    <First />
-    <WithParameter title="second component" subtitle="Very nice!" />
-    <Fragment />
+    <div className="content">
+      <Card title="Random numbers">
+        <Random min={1} max={5} />
+      </Card>
+
+      <Card title="Fragment">
+        <Fragment />
+      </Card>
+
+      <Card title="With parameter">
+        <WithParameter title="second component" subtitle="Very nice!" />
+      </Card>
+
+      <Card title="First component">
+        <First />
+      </Card>
+    </div>
   </div>
 );
 
